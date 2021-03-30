4ff4d69c097a1d3650d0c63a6519e5ccd32038fc
Jonatas Almeida <jonatas-almeida@github.com>
2021-03-30 03:17:19 -0300

    Criação dos métodos do Service

---
+++ src/app/interfaces/Favorites.ts
@@ -0,0 +1,9 @@
+import { Users } from "./Users";
+
+export interface Favorites {
+
+    id: number;
+    user: Users[];
+    message: string;
+
+}
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contacts } from '../interfaces/Contacts';
+import { Favorites } from '../interfaces/favorites';
 import { Messages } from '../interfaces/Messages';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { Messages } from '../interfaces/Messages';
 })
 export class ChatService {
 
-  baseUrl = "http://localhost:3000"
+  baseUrl = "http://localhost:3000";
 
   constructor(private http: HttpClient) { }
 
@@ -23,8 +24,45 @@ export class ChatService {
     return this.http.post(`${this.baseUrl}/messages`, message);
   }
 
+  //Deleta a mensagem
+  deleteMessage(id: number){
+    return this.http.delete(`${this.baseUrl}/messages/${id}`);
+  }
+
+  //---------------------------------------------------------------------------------------
+
+  //Pega os contatos adicionados
+  getContacts(): Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}/contacts`);
+  }
+
   //Salva os contatos para serem acessados posteriormente
   postContacts(contact: Contacts) {
     return this.http.post(`${this.baseUrl}/contacts`, contact);
   }
+
+  //Edita os contatos salvos
+  editContacts(contact: Contacts, id: number){
+    return this.http.put(`${this.baseUrl}/contacts/${id}`, contact);
+  }
+
+  //Deleta os contatos adicionados
+  deleteContacts(id: number){
+    return this.http.delete(`${this.baseUrl}/contacts/${id}`);
+  }
+
+  //---------------------------------------------------------------------------------------
+
+  //Move a mensagem marcada como favorita para a parte de favoritos
+  postFavorites(favorite: Favorites){
+    for(favorite.id = 0; favorite.id == favorite.id; favorite.id + 1){
+      return this.http.post(`${this.baseUrl}/favorites`, favorite);
+    }
+  }
+
+  //Dispensar dos favoritos
+  deleteFavorites(id: number){
+    return this.http.delete(`${this.baseUrl}/favorites/${id}`);
+  }
+
 }
